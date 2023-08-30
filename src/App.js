eec826bb1bac32003e758175f6b0b99f012e6812
JARED HENRY <109435235+J-henry1@users.noreply.github.com>
2023-08-30 13:06:52 -0400

    Add to oder queue complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Axios from "axios";
 
 function App() {
   
-  const [listOfProducts, setListOfProducts] = useState ([])
+  const [listOfProducts, setListOfProducts] = useState ([]);
+  const[listOfOrders, setListOfOrders] = useState ([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
   
   //makes api call to backend
   useEffect(() =>{
@@ -18,18 +20,50 @@ function App() {
 
   }, []);
 
+
+
+  function submitToQueue(product) {
+    console.log(product.productName);
+    console.log("New");
+    console.log(listOfOrders + 1);
+    console.log(new Date());
+    
+
+    const order = {
+      orderNumber: listOfOrders.length + 1, 
+      productsName: product.productName,
+      orderStatus: "New",
+      // Assuming orderNumber starts from 1
+      orderDate: new Date()
+    };
+
+    Axios.post("http://localhost:3001/createOrders", order)
+    .then((response) => {
+      console.log("Order submitted:", response.data);
+      // Update the list of orders
+      setListOfOrders((prevOrders) => [...prevOrders, response.data]);
+    })
+    .catch((error) => {
+      console.error("Error submitting order:", error);
+    });
+}//end submit to queue
+
   return (
     <div className="App">
 
-      <div class="products">
+      <div className="products">
 
           {listOfProducts.map((product) => {
-              return (<div>
-
+              return (<div key={product._id}>
                     <h1>Product Name: {product.productName}</h1>
                     <h1>Product Description: {product.productDescription}</h1>
                     <h1>Product Price: ${product.price}</h1>
-
+                    <button type = "button"
+                    onClick={() => {
+                      setSelectedProduct(product); // Set the selected product
+                      submitToQueue(product); // Submit the product to the order collection
+                    }}
+                    >Order Now</button>
               </div>
             );
           })}
